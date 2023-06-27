50769a3742a2f8efe764f0e3883b93753833effd
waley-code <oyewaleadeosun@yahoo.com>
2023-06-27 04:45:01 +0100

    complet mandatory

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,19 +1,17 @@
-//Comments for ES6
-export function taskFirst(){
-    const task = 'I prefer const when I can.';
+// Comments for ES6
+export function taskFirst() {
+  const task = 'I prefer const when I can.';
 
-    return task;
+  return task;
 }
 
-
-export  function getLast(){
-    return ' is okay';
+export function getLast() {
+  return ' is okay';
 }
 
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
 
-export  function taskNext(){
-    let combination = 'But sometimes let';
-    combination += getLast();
-
-    return combination;
+  return combination;
 }
