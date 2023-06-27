23950cbb3cad0a64e92c3f7084f96b2dac045a24
waley-code <oyewaleadeosun@yahoo.com>
2023-06-27 16:16:53 +0100

    fix eslint errors

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,10 +1,10 @@
 export default function createIteratorObject(report) {
-    const x = Object.values(report);
-    let y = [];
-    let re = [];
-    x.map(c => y.push(...(Object.values(c))));
-    for(let s of y){
-        re.push(...s)
-    }
-    return re;
+  const x = Object.values(report);
+  const y = [];
+  const re = [];
+  x.map((c) => y.push(...(Object.values(c))));
+  for (const s of y) {
+    re.push(...s);
+  }
+  return re;
 }
--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,7 +1,7 @@
 export default function iterateThroughObject(reportWithIterator) {
-    let st = "";
-    for (let index = 0; index < reportWithIterator.length; index++) {
-        st += reportWithIterator[index] +(( index ==(reportWithIterator.length - 1))? "": " | ");
-    }
- return st;
+  let st = '';
+  for (let index = 0; index < reportWithIterator.length; index += 1) {
+    st += reportWithIterator[index] + ((index === (reportWithIterator.length - 1)) ? '' : ' | ');
+  }
+  return st;
 }
