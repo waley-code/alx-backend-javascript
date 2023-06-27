e275f58dcecc1eb6ac9e9a5b7cd3abf149364c5e
waley-code <oyewaleadeosun@yahoo.com>
2023-06-27 02:39:26 +0100

    adjusted syntax

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,12 +1,15 @@
- export function taskFirst(){
+//Comments for ES6
+export function taskFirst(){
     const task = 'I prefer const when I can.';
 
     return task;
 }
 
+
 export  function getLast(){
     return ' is okay';
 }
+
 
 export  function taskNext(){
     let combination = 'But sometimes let';
