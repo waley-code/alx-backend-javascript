6b59765ef89e4fd9dec7709ca41b1c3fb6cdbb69
waley-code <oyewaleadeosun@yahoo.com>
2023-08-21 23:45:42 +0100

    fix semistandard

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,13 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on("end", () => {
-  process.stdout.write("This important software is now closing\n");
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
 
-process.stdin.on("readable", () => {
+process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
 
   if (chunk) {
     process.stdout.write(`Your name is: ${chunk}`);
   }
 });
-
