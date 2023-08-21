683dd641b4c856898784efbf35fbc95817bcc6a3
waley-code <oyewaleadeosun@yahoo.com>
2023-08-22 00:01:21 +0100

    fix task 4 semistandard

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,14 +1,14 @@
-const http = require("http");
+const http = require('http');
 
 const PORT = 1245;
-const HOST = "localhost";
+const HOST = 'localhost';
 const app = http.createServer();
 
-app.on("request", (_, res) => {
-  const responseText = "Hello Holberton School!";
+app.on('request', (_, res) => {
+  const responseText = 'Hello Holberton School!';
 
-  res.setHeader("Content-Type", "text/plain");
-  res.setHeader("Content-Length", responseText.length);
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', responseText.length);
   res.statusCode = 200;
   res.write(Buffer.from(responseText));
 });
