2fc537f364d2199f3c0f9f124b83794b4030e94c
waley-code <oyewaleadeosun@yahoo.com>
2023-08-21 23:39:45 +0100

    coplete task 0

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Adeosun Oyewale <https://github.com/waley_code>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
