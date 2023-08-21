a2e43defa31ca52608a4b97bb52f24d8b2ee6f6c
waley-code <oyewaleadeosun@yahoo.com>
2023-08-22 00:10:43 +0100

    fix all

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const fs = require("fs");
+const express = require('express');
+const fs = require('fs');
 
 const app = express();
 const PORT = 1245;
-const DB_FILE = process.argv.length > 2 ? process.argv[2] : "";
+const DB_FILE = process.argv.length > 2 ? process.argv[2] : '';
 
 /**
  * Counts the students in a CSV data file.
@@ -13,25 +13,25 @@ const DB_FILE = process.argv.length > 2 ? process.argv[2] : "";
 const countStudents = (dataPath) =>
   new Promise((resolve, reject) => {
     if (!dataPath) {
-      reject(new Error("Cannot load the database"));
+      reject(new Error('Cannot load the database'));
     }
     if (dataPath) {
       fs.readFile(dataPath, (err, data) => {
         if (err) {
-          reject(new Error("Cannot load the database"));
+          reject(new Error('Cannot load the database'));
         }
         if (data) {
           const reportParts = [];
-          const fileLines = data.toString("utf-8").trim().split("\n");
+          const fileLines = data.toString('utf-8').trim().split('\n');
           const studentGroups = {};
-          const dbFieldNames = fileLines[0].split(",");
+          const dbFieldNames = fileLines[0].split(',');
           const studentPropNames = dbFieldNames.slice(
             0,
             dbFieldNames.length - 1
           );
 
           for (const line of fileLines.slice(1)) {
-            const studentRecord = line.split(",");
+            const studentRecord = line.split(',');
             const studentPropValues = studentRecord.slice(
               0,
               studentRecord.length - 1
@@ -42,7 +42,7 @@ const countStudents = (dataPath) =>
             }
             const studentEntries = studentPropNames.map((propName, idx) => [
               propName,
-              studentPropValues[idx],
+              studentPropValues[idx]
             ]);
             studentGroups[field].push(Object.fromEntries(studentEntries));
           }
@@ -55,38 +55,38 @@ const countStudents = (dataPath) =>
             reportParts.push(
               [
                 `Number of students in ${field}: ${group.length}.`,
-                "List:",
-                group.map((student) => student.firstname).join(", "),
-              ].join(" ")
+                'List:',
+                group.map((student) => student.firstname).join(', ')
+              ].join(' ')
             );
           }
-          resolve(reportParts.join("\n"));
+          resolve(reportParts.join('\n'));
         }
       });
     }
   });
 
-app.get("/", (_, res) => {
-  res.send("Hello Holberton School!");
+app.get('/', (_, res) => {
+  res.send('Hello Holberton School!');
 });
 
-app.get("/students", (_, res) => {
-  const responseParts = ["This is the list of our students"];
+app.get('/students', (_, res) => {
+  const responseParts = ['This is the list of our students'];
 
   countStudents(DB_FILE)
     .then((report) => {
       responseParts.push(report);
-      const responseText = responseParts.join("\n");
-      res.setHeader("Content-Type", "text/plain");
-      res.setHeader("Content-Length", responseText.length);
+      const responseText = responseParts.join('\n');
+      res.setHeader('Content-Type', 'text/plain');
+      res.setHeader('Content-Length', responseText.length);
       res.statusCode = 200;
       res.write(Buffer.from(responseText));
     })
     .catch((err) => {
       responseParts.push(err instanceof Error ? err.message : err.toString());
-      const responseText = responseParts.join("\n");
-      res.setHeader("Content-Type", "text/plain");
-      res.setHeader("Content-Length", responseText.length);
+      const responseText = responseParts.join('\n');
+      res.setHeader('Content-Type', 'text/plain');
+      res.setHeader('Content-Length', responseText.length);
       res.statusCode = 200;
       res.write(Buffer.from(responseText));
     });
