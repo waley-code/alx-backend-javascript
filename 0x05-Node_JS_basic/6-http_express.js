82714f246fdcf53bf82c24a2aaf9c4f55c34a135
waley-code <oyewaleadeosun@yahoo.com>
2023-08-22 00:07:11 +0100

    fix task 6

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,10 +1,10 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 const PORT = 1245;
 
-app.get("/", (_, res) => {
-  res.send("Hello Holberton School!");
+app.get('/', (_, res) => {
+  res.send('Hello Holberton School!');
 });
 
 app.listen(PORT, () => {
