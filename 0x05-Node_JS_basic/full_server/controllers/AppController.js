ae713b4279981b50ba683fe014cf1e74bd625d5a
waley-code <oyewaleadeosun@yahoo.com>
2023-08-22 19:38:55 +0100

    complete controllers

---
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -0,0 +1,10 @@
+
+
+class AppController {
+  static getHomepage(request, response) {
+    response.status(200).send("Hello Holberton School!");
+  }
+}
+
+export default AppController;
+module.exports = AppController;
