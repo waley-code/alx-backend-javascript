1b4c18fdb13bc3cf7af0bdb73fa2985920fd31bd
waley-code <oyewaleadeosun@yahoo.com>
2023-08-23 17:14:50 +0100

    fix sever.js

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -6,7 +6,7 @@ const PORT = 1245;
 
 mapRoutes(app);
 app.listen(PORT, () => {
-  // console.log(`Server listening on PORT ${PORT}`);
+  console.log(`Server listening on PORT ${PORT}`);
 });
 
 export default app;
